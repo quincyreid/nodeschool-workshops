f5692439bf2ef890a844bf99d287b45432d7b1a6
Quincy Reid <qreid51@gmail.com>
2017-01-18 18:18:52 -0500

    Adds http.js

---
+++ learnyounode/lesson/http.js
@@ -0,0 +1,15 @@
+const http = require('http')
+const url = process.argv[2]
+
+http.get(url, (res) => {
+  res.setEncoding('utf8')
+  res.on('data', (data) => {
+    console.log(data)
+  })
+})
+
+// http.get(process.argv[2], function (response) {
+//   response.setEncoding('utf8')
+//   response.on('data', console.log)
+//   response.on('error', console.error)
+// }).on('error', console.error)
